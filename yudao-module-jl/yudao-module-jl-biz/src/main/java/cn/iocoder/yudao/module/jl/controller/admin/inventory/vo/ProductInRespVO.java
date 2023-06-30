9f14380a0b7f92b946484a20b41afa3f4f70c9b6
yun36524 <yun36524@gmail.com>
2023-06-30 20:14:30 +0800

    feat: ProductIn级联project creator

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/inventory/vo/ProductInRespVO.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/inventory/vo/ProductInRespVO.java
@@ -1,6 +1,8 @@
 package cn.iocoder.yudao.module.jl.controller.admin.inventory.vo;
 
 import cn.iocoder.yudao.module.jl.entity.inventory.ProductInItem;
+import cn.iocoder.yudao.module.jl.entity.project.ProjectOnly;
+import cn.iocoder.yudao.module.jl.entity.user.User;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.*;
 import java.time.LocalDateTime;
@@ -18,5 +20,7 @@ public class ProductInRespVO extends ProductInBaseVO {
     @Schema(description = "创建时间", requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime createTime;
 
+    private ProjectOnly project;
+    private User user;
     private List<ProductInItemRespVO> items;
 }
