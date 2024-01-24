1dee6aa1a26df82a50c65e586ff28f12cd4316f0
yun36524 <yun36524@gmail.com>
2024-01-24 15:03:18 +0800

    fix: 报价返回更新时间

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/projectquotation/vo/ProjectQuotationRespVO.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/projectquotation/vo/ProjectQuotationRespVO.java
@@ -16,4 +16,6 @@ public class ProjectQuotationRespVO extends ProjectQuotationBaseVO {
     @Schema(description = "创建时间", requiredMode = Schema.RequiredMode.REQUIRED)
     private LocalDateTime createTime;
 
+    @Schema(description = "更新时间", requiredMode = Schema.RequiredMode.REQUIRED)
+    private LocalDateTime updateTime;
 }
