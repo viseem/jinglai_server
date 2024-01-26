cb422fd780c460a21b7b195ab0799885ccbe97bb
yun36524 <yun36524@gmail.com>
2024-01-26 17:34:15 +0800

    fix: 项目物资列表查询问题，屏蔽project code为null的

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/project/vo/ProjectSupplyPageReqVO.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/project/vo/ProjectSupplyPageReqVO.java
@@ -15,6 +15,8 @@ import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_
 @ToString(callSuper = true)
 public class ProjectSupplyPageReqVO extends PageParam {
 
+    private Boolean noQuotationItem;
+
     @Schema(description = "创建时间")
     @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
     private LocalDateTime[] createTime;
