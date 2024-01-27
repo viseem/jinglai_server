0880177a8d5b33fe1d1c519d16febad83b3c68d7
yun36524 <yun36524@gmail.com>
2024-01-27 10:49:21 +0800

    feat: 统计数据的基础请求类

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/statistic/vo/StatisticBaseVO.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/statistic/vo/StatisticBaseVO.java
@@ -1,23 +1,24 @@
-package cn.iocoder.yudao.module.jl.controller.admin.subjectgroup.vo.subgroupworkstation;
+package cn.iocoder.yudao.module.jl.controller.admin.statistic.vo;
 
-import cn.iocoder.yudao.framework.common.pojo.PageParam;
 import io.swagger.v3.oas.annotations.media.Schema;
 import lombok.Data;
-import lombok.EqualsAndHashCode;
-import lombok.ToString;
 import org.springframework.format.annotation.DateTimeFormat;
 
 import java.time.LocalDateTime;
 
 import static cn.iocoder.yudao.framework.common.util.date.DateUtils.FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND;
 
-@Schema(description = "专题组 - 统计数据 - 基础信息 VO")
+@Schema(description = "统计数据 - 基础请求 VO")
 @Data
-public class SubjectGroupStatsBaseVO {
+public class StatisticBaseVO {
 
-    @Schema(description = "起止时间")
+    @Schema(description = "开始日期")
     @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
-    private LocalDateTime[] createTime;
+    private LocalDateTime startTime;
+
+    @Schema(description = "结束日期")
+    @DateTimeFormat(pattern = FORMAT_YEAR_MONTH_DAY_HOUR_MINUTE_SECOND)
+    private LocalDateTime endTime;
 
     @Schema(description = "人员id数组", example = "27395")
     private Long[] userIds;
@@ -25,4 +26,6 @@ public class SubjectGroupStatsBaseVO {
     @Schema(description = "专题组id", example = "27395")
     private Long subjectGroupId;
 
+    @Schema(description = "用户id", example = "27395")
+    private Long userId;
 }
