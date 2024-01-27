a8944d3b0afe00b1369ead51af5384f008383c27
yun36524 <yun36524@gmail.com>
2024-01-27 10:34:04 +0800

    feat：专题组统计空的service

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/subjectgroup/SubjectGroupStatsController.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/controller/admin/subjectgroup/SubjectGroupStatsController.java
@@ -3,9 +3,8 @@ package cn.iocoder.yudao.module.jl.controller.admin.subjectgroup;
 import cn.iocoder.yudao.framework.common.pojo.CommonResult;
 import cn.iocoder.yudao.module.jl.controller.admin.subjectgroup.vo.*;
 import cn.iocoder.yudao.module.jl.controller.admin.subjectgroup.vo.subgroupworkstation.SubjectGroupFollowupStatsReqVO;
-import cn.iocoder.yudao.module.jl.entity.subjectgroup.SubjectGroup;
 import cn.iocoder.yudao.module.jl.mapper.subjectgroup.SubjectGroupMapper;
-import cn.iocoder.yudao.module.jl.service.subjectgroup.SubjectGroupWorkstationService;
+import cn.iocoder.yudao.module.jl.service.subjectgroup.SubjectGroupStatsService;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.Parameter;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -15,11 +14,9 @@ import org.springframework.web.bind.annotation.*;
 
 import javax.annotation.Resource;
 import javax.validation.Valid;
-import java.util.Optional;
 
 import static cn.iocoder.yudao.framework.common.exception.util.ServiceExceptionUtil.exception;
 import static cn.iocoder.yudao.framework.common.pojo.CommonResult.success;
-import static cn.iocoder.yudao.module.jl.enums.ErrorCodeConstants.SUBJECT_GROUP_NOT_EXISTS;
 
 @Tag(name = "管理后台 - 专题小组")
 @RestController
@@ -28,7 +25,7 @@ import static cn.iocoder.yudao.module.jl.enums.ErrorCodeConstants.SUBJECT_GROUP_
 public class SubjectGroupStatsController {
 
     @Resource
-    private SubjectGroupWorkstationService subjectGroupWorkstationService;
+    private SubjectGroupStatsService subjectGroupStatsService;
 
     @Resource
     private SubjectGroupMapper subjectGroupMapper;
