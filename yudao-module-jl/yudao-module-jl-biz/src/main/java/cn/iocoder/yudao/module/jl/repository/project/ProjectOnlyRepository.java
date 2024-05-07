929b372188ebe369c0fdcfc906f90f54b65d4162
yun36524 <yun36524@gmail.com>
2024-05-07 10:18:06 +0800

    fix: 调整主动取消审批流程逻辑--

--- yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/repository/project/ProjectOnlyRepository.java
+++ yudao-module-jl/yudao-module-jl-biz/src/main/java/cn/iocoder/yudao/module/jl/repository/project/ProjectOnlyRepository.java
@@ -10,17 +10,22 @@ import org.springframework.transaction.annotation.Transactional;
 import java.util.List;
 
 /**
-* ProjectRepository
-*
-*/
+ * ProjectRepository
+ */
 public interface ProjectOnlyRepository extends JpaRepository<ProjectOnly, Long>, JpaSpecificationExecutor<ProjectOnly> {
     @Transactional
     @Modifying
     @Query("update ProjectOnly p set p.processInstanceId = ?1, p.outboundApplyResult = ?2 where p.id = ?3")
     int updateProcessInstanceIdAndOutboundApplyResultById(String processInstanceId, String outboundApplyResult, Long id);
 
+    @Transactional
+    @Modifying
+    @Query("update ProjectOnly p set  p.outboundApplyResult = ?1 where p.id = ?2")
+    int updateOutboundApplyResultById(String outboundApplyResult, Long id);
+
     @Query("select p from ProjectOnly p where p.salesId in ?1 and p.code is not null and p.managerId not in ?2")
     List<ProjectOnly> findBySalesIdInAndCodeNotNullAndManagerIdNotIn(List<Long> salesIds, List<Long> managerIds);
+
     @Query("select p from ProjectOnly p where p.managerId in ?1 and p.code is not null")
     List<ProjectOnly> findByInManagerIdAndCodeNotNull(Long[] managerIds);
 
